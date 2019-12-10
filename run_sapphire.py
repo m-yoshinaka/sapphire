e13584c145387ffe0556b49c504b10ee33a10769
mybon13 <49668018+m-yoshinaka@users.noreply.github.com>
2019-12-10 17:16:22 +0900

    update some files

--- run_sapphire.py
+++ run_sapphire.py
@@ -4,31 +4,40 @@ from sapphire import Sapphire
 def run_sapphire():
     aligner = Sapphire()
 
-    sentence_src = ""
-    while sentence_src == "":
-        print("Input sentence1")
-        sentence_src = input(">> ")
-    sentence_trg = ""
-    while sentence_trg == "":
-        print("Input sentence2")
-        sentence_trg = input(">> ")
+    while True:
 
-    tokens_src = sentence_src.split()
-    tokens_trg = sentence_trg.split()
+        sentence_src = ""
+        while sentence_src == "":
+            print("Input sentence1")
+            sentence_src = input("> ")
+        if sentence_src == "exit":
+            break
 
-    alignment = aligner.align(tokens_src, tokens_trg)
-    print("{}\n".format(alignment))
+        sentence_trg = ""
+        while sentence_trg == "":
+            print("Input sentence2")
+            sentence_trg = input("> ")
+        if sentence_trg == "exit":
+            break
 
-    if "-" in alignment:
-        for al in alignment.split(" "):
-            srcs, trgs = al.split("-")
-            src_s = int(srcs.split(",")[0])
-            src_e = int(srcs.split(",")[-1])
-            trg_s = int(trgs.split(",")[0])
-            trg_e = int(trgs.split(",")[-1])
-            text_src = " ".join(tokens_src[src_s - 1:src_e])
-            text_trg = " ".join(tokens_trg[trg_s - 1:trg_e])
-            print("{} <--> {}".format(text_src, text_trg))
+        tokens_src = sentence_src.split()
+        tokens_trg = sentence_trg.split()
+
+        alignment = aligner.align(tokens_src, tokens_trg)
+        print("\n{}\n".format(alignment))
+
+        if "-" in alignment:
+            for al in alignment.split(" "):
+                srcs, trgs = al.split("-")
+                src_s = int(srcs.split(",")[0])
+                src_e = int(srcs.split(",")[-1])
+                trg_s = int(trgs.split(",")[0])
+                trg_e = int(trgs.split(",")[-1])
+                text_src = " ".join(tokens_src[src_s - 1:src_e])
+                text_trg = " ".join(tokens_trg[trg_s - 1:trg_e])
+                print("{} <--> {}".format(text_src, text_trg))
+        
+        print("---")
 
 
 if __name__ == "__main__":
