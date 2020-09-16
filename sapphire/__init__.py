69fb084a655a6964770a0f9bf40fa7c04310bc37
m-yoshinaka <49668018+m-yoshinaka@users.noreply.github.com>
2020-09-16 17:24:15 +0000

    [update] Add some modules

--- sapphire/__init__.py
+++ sapphire/__init__.py
@@ -1,1 +1,5 @@
-from .sapphire import Sapphire
+from sapphire.sapphire import Sapphire
+from sapphire.word_alignment import (
+    WordEmbedding, FastTextVectorize, WordAlign, get_similarity_matrix
+)
+from sapphire.phrase_alignment import PhraseExtract, PhraseAlign
