3ea5a3ed514ef16b734d77e0bccd50eec59dc37f
m-yoshinaka <49668018+m-yoshinaka@users.noreply.github.com>
2020-09-16 17:32:45 +0000

    [update] Change the behavior of WordAlign class - Separate get_similarity_matrix - An instance of WordAlign has some parameters as variables - Use __call__ method

--- sapphire/sapphire.py
+++ sapphire/sapphire.py
@@ -1,34 +1,33 @@
-from .word_alignment import FastTextVectorize, WordAlign
-from .phrase_alignment import PhraseExtract, PhraseAlign
+from sapphire.word_alignment import (
+    FastTextVectorize, WordAlign, get_similarity_matrix
+)
+from sapphire.phrase_alignment import PhraseExtract, PhraseAlign
 
 
 class Sapphire(object):
 
     def __init__(self, model):
         self.vectorizer = FastTextVectorize(model)
-        self.word_aligner = WordAlign()
+        self.set_params()
+        self.word_aligner = WordAlign(self.lambda_, self.use_hungarian)
         self.extractor = PhraseExtract()
         self.phrase_aligner = PhraseAlign()
-        self.set_params()
 
     def set_params(self, lambda_=0.6, delta=0.6, alpha=0.01, hungarian=False):
         self.lambda_ = lambda_
         self.delta = delta
         self.alpha = alpha
-        self.hungarian = hungarian
+        self.use_hungarian = hungarian
 
     def align(self, tokens_src: list, tokens_trg: list):
         len_src = len(tokens_src)
         len_trg = len(tokens_trg)
 
-        vectors_src = self.vectorizer.vectorize(tokens_src)
-        vectors_trg = self.vectorizer.vectorize(tokens_trg)
+        vectors_src = self.vectorizer(tokens_src)
+        vectors_trg = self.vectorizer(tokens_trg)
 
-        sim_matrix = self.word_aligner.similarity_matrix(vectors_src,
-                                                         vectors_trg)
-        word_alignment = self.word_aligner.align(sim_matrix,
-                                                 self.lambda_,
-                                                 self.hungarian)
+        sim_matrix = get_similarity_matrix(vectors_src, vectors_trg)
+        word_alignment = self.word_aligner(sim_matrix)
 
         phrase_pairs = self.extractor.extract(word_alignment,
                                               vectors_src,
--- sapphire/word_alignment.py
+++ sapphire/word_alignment.py
@@ -1,7 +1,7 @@
-import itertools
 import numpy as np
+from itertools import product
+from scipy.spatial.distance import cosine
 from scipy.optimize import linear_sum_assignment
-from scipy.spatial import distance
 
 
 class WordEmbedding(object):
@@ -9,7 +9,7 @@ class WordEmbedding(object):
     def __init__(self):
         pass
 
-    def vectorize(self, word: list) -> np.array:
+    def __call__(self, words):
         pass
 
 
@@ -20,7 +20,7 @@ class FastTextVectorize(WordEmbedding):
         self.model = model
         self.dim = model.get_dimension()
 
-    def vectorize(self, words: list) -> np.array:
+    def __call__(self, words):
         vector = []
 
         if words:
@@ -32,32 +32,37 @@ class FastTextVectorize(WordEmbedding):
         return np.array(vector)
 
 
+def get_similarity_matrix(src_vectors, trg_vectors):
+    len_src = len(src_vectors)
+    len_trg = len(trg_vectors)
+    sim_matrix = np.zeros((len_src, len_trg))
+
+    for (src_idx, src_vec), (trg_idx, trg_vec) in product(
+        enumerate(src_vectors), enumerate(trg_vectors)
+    ):
+        sim_matrix[src_idx][trg_idx] = 1 - cosine(src_vec, trg_vec)
+
+    return sim_matrix
+
+
 class WordAlign(object):
 
-    def __init__(self):
-        self.name = ''
+    def __init__(self, lambda_, use_hungarian):
+        self.lambda_ = lambda_
+        self.use_hungarian = use_hungarian
 
-    @staticmethod
-    def similarity_matrix(vectors_src: np.array, vectors_trg: np.array) -> np.ndarray:
-        len_src = len(vectors_src)
-        len_trg = len(vectors_trg)
+    def __call__(self, sim_matrix):
+        if self.use_hungarian:
+            alignments = self._hungarian_assign(sim_matrix)
+        else:
+            alignments = self._grow_diag_final(sim_matrix)
 
-        sim_matrix = np.zeros((len_src, len_trg))
-
-        for (id_src, vec_src), (id_trg, vec_trg) in itertools.product(enumerate(vectors_src), enumerate(vectors_trg)):
-            sim_matrix[id_src][id_trg] = 1 - distance.cosine(vec_src, vec_trg)
-
-        return sim_matrix
+        return [(s + 1, t + 1) for s, t in alignments
+                if sim_matrix[s][t] >= self.lambda_]
 
     @staticmethod
     def _hungarian_assign(sim_matrix):
-
-        len_src, len_trg = sim_matrix.shape
-        cost_matrix = np.ones((len_src, len_trg))
-
-        for id_src, id_trg in itertools.product(range(len_src), range(len_trg)):
-            cost_matrix[id_src][id_trg] -= sim_matrix[id_src][id_trg]
-
+        cost_matrix = - sim_matrix
         aligned_src, aligned_trg = linear_sum_assignment(cost_matrix)
         alignments = [(s, t) for s, t in zip(aligned_src, aligned_trg)]
 
@@ -68,13 +73,15 @@ class WordAlign(object):
 
         def _grow_diag():
             point_added = False
-            for src, trg in itertools.product(range(len_src), range(len_trg)):
+            for src, trg in product(range(len_src), range(len_trg)):
                 if not align_matrix[src][trg]:
                     continue
                 for ns, nt in neighbors:
-                    if src + ns < 0 or src + ns >= len_src or trg + nt < 0 or trg + nt >= len_trg:
+                    if src + ns < 0 or src + ns >= len_src \
+                            or trg + nt < 0 or trg + nt >= len_trg:
                         continue
-                    if (not np.any(align_matrix[src + ns, :]) or not np.any(align_matrix[:, trg + nt])) \
+                    if (not np.any(align_matrix[src + ns, :])
+                        or not np.any(align_matrix[:, trg + nt])) \
                             and union_matrix[src + ns][trg + nt]:
                         align_matrix[src + ns][trg + nt] = 1
                         point_added = True
@@ -82,8 +89,9 @@ class WordAlign(object):
                 _grow_diag()
 
         def _final(matrix):
-            for src, trg in itertools.product(range(len_src), range(len_trg)):
-                if (not np.any(align_matrix[src, :]) or not np.any(align_matrix[:, trg])) \
+            for src, trg in product(range(len_src), range(len_trg)):
+                if (not np.any(align_matrix[src, :])
+                    or not np.any(align_matrix[:, trg])) \
                         and matrix[src][trg]:
                     align_matrix[src][trg] = 1
 
@@ -107,17 +115,8 @@ class WordAlign(object):
         _final(trg2src)
 
         alignments = []
-        for s, t in itertools.product(range(len_src), range(len_trg)):
+        for s, t in product(range(len_src), range(len_trg)):
             if align_matrix[s][t]:
                 alignments.append((s, t))
 
         return alignments
-
-    def align(self, sim_matrix: np.ndarray, gamma, hungarian) -> list:
-        ### 1-index alignment ###
-        if hungarian:
-            alignments = self._hungarian_assign(sim_matrix)
-        else:
-            alignments = self._grow_diag_final(sim_matrix)
-
-        return [(s + 1, t + 1) for s, t in alignments if sim_matrix[s][t] >= gamma]
