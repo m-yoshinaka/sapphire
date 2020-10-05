3390406e361f8a29569de7e3edd8b03f8fff6e88
m-yoshinaka <49668018+m-yoshinaka@users.noreply.github.com>
2020-10-05 03:28:59 +0000

    [remove] Remove sapphire/setting.py

--- sapphire/setting.py
+++
@@ -1,5 +0,0 @@
-HUGARIAN = False  # word alignment option (default: grow-diag-final)
-
-LAMBDA = 0.7  # threshold of word alignment candidate score
-DELTA = 0.9  # threshold of phrase alignment candidate score
-ALPHA = 0.05  # bias for length of phrase
